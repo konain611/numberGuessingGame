e365d71c2a36c7aabc80daf486474468b39e5780
Syed Konain Nasir <konain611@gmail.com>
2024-05-22 00:09:29 +0500

    range_added

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please Guess a number",
+        message: "Please Guess a number between 1 - 10:",
     },
 ]);
-if (answer.userGuessedNumber === randomNumber) {
+if (answer.userGuessedNumber > 10) {
+    console.log("Please guess the number between the given range.");
+}
+else if (answer.userGuessedNumber === randomNumber) {
     console.log("Congratulations. You Guessed the right number. YAYY");
+    console.log(`The Random number was actually ${randomNumber} :D `);
 }
 else {
     console.log("You guessed the wrong number :( ");
+    console.log(`The Random number was ${randomNumber}`);
 }
-console.log(`The Random number was ${randomNumber}`);
