fb4097ef31f88f2e222d826fbcf0ae3978113d05
Syed Konain Nasir <konain611@gmail.com>
2024-05-22 00:25:40 +0500

    update version

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const answer = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please Guess a number between 1 - 10:",
+        message: "Please Guess a number between 1-10:",
     },
 ]);
 
@@ -20,10 +20,10 @@ if(answer.userGuessedNumber > 10){
 
 else if(answer.userGuessedNumber === randomNumber){
     console.log("Congratulations. You Guessed the right number. YAYY");
-    console.log(`The Random number was actually ${randomNumber} :D `);
+    console.log(`The Random number was actually ${randomNumber} :D`);
 }
 else{
-    console.log("You guessed the wrong number :( ");
+    console.log("You guessed the wrong number :(");
     console.log(`The Random number was ${randomNumber}`);
 }
 
